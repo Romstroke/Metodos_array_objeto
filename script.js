ec8c8979011c24f9a0e7f2b2cf349a070e0303ca
Romina <rominchuela@gmail.com>
2024-02-20 15:23:30 -0300

    Implemento función que imprime total manipulaciones, ordeno y comento código

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.write('<p>Estadisticas centro medico ñuñoa</p>');
+// document.write('<p>Estadisticas centro medico ñuñoa</p>');
 
 const radiologia = [
     { hora: '11:00', especialista: 'IGNACIO SCHULZ', paciente: 'FRANCISCA ROJAS', rut: '9878782-1', prevision: 'FONASA' },
@@ -29,11 +29,11 @@ const dental = [
 
 //Agregar código para el desafio 2 aquí
 
-//copias de arrays para no trabajar sobre el original y asi no arriesgarme a eliminar algo
+//Copias de arrays para no trabajar sobre el original y asi no arriesgarme a eliminar algo
 // const copiaRadiologia = [radiologia] //aqui se agregan despues del array
 // const copiaDental = dental; //referencia 
 const copiaRadiologia = [...radiologia];
-const copiaTraumatologia = [...traumatologia]; //... 
+const copiaTraumatologia = [...traumatologia]; //... operador spread toma todo y lo une a los nuevos elementos agregados, y todo en un mismo array 
 const copiaDental = [...dental];
 
 //1. Agregar las siguientes horas al arreglo de Traumatología (2 Puntos):  
@@ -74,53 +74,118 @@ copiaTraumatologia.push(
         rut: '14345656-6',
         prevision: 'FONASA'
     },
-)
-console.log(traumatologia);
-console.log(copiaTraumatologia);
+);
+
+//Para comprobar que se esté manipulando la copia y no el original
+// console.log(traumatologia);
+// console.log(copiaTraumatologia);
 
 //2. Eliminar el primer y último elemento del arreglo de Radiología (1 Punto). 
 
 copiaRadiologia.pop();
 copiaRadiologia.shift();
 
-console.log(radiologia);
-console.log(copiaRadiologia);
+//Para comprobar que se esté manipulando la copia y no el original
+// console.log(radiologia);
+// console.log(copiaRadiologia);
 
 // 3. Imprimir en la página HTML, mediante document.write y/o las funciones que estime conveniente, 
 //la lista de consultas médicas de Dental. Sin embargo, debe hacerlo separando por un guión cada dato desplegado
 //y cada fila de información debe estar separada por un párrafo (2 Puntos).  
 
-for (let i = 0; i < copiaDental.length; i++) {
-    document.write(`<p>${copiaDental[i].hora} - ${copiaDental[i].especialista} - ${copiaDental[i].paciente} -  ${copiaDental[i].rut} -  ${copiaDental[i].prevision} </p>`);
-}
+// for (let i = 0; i < copiaDental.length; i++) {
+//     document.write(`<p>${copiaDental[i].hora} - ${copiaDental[i].especialista} - ${copiaDental[i].paciente} -  ${copiaDental[i].rut} -  ${copiaDental[i].prevision} </p>`);
+// }
 
 //4. Imprimir un listado total de todos los pacientes que se atendieron en el centro médico.  
 //Para esto, deberá unir todos los nombres de pacientes e imprimir uno por cada párrafo (3 Puntos).  
 
-const pacientesRadiologia = copiaRadiologia.map(function(paciente){
+const pacientesRadiologia = copiaRadiologia.map(function (paciente) {
     return paciente.paciente;
 });
 
-const pacientesTraumatologia = copiaTraumatologia.map(function(paciente){
+const pacientesTraumatologia = copiaTraumatologia.map(function (paciente) {
     return paciente.paciente;
 });
 
-const pacientesDental = copiaDental.map(function(paciente){
+const pacientesDental = copiaDental.map(function (paciente) {
     return paciente.paciente;
 });
 
 const pacientesTotal = pacientesRadiologia.concat(pacientesTraumatologia, pacientesDental);
 
-console.log(pacientesTotal)
+// console.log(typeof pacientesTotal)
+// console.log(pacientesTotal.length)
+
+//Anteriormente en ejercicio 2, se ha eliminado a FRANCISCA ROJAS y RAMON ULLOA(por estar dentro del primer y último objeto del array)
+
+// for(let i=0;i < pacientesTotal.length;i++){
+//     document.write(`<p>${pacientesTotal[i]}</p>`)
+// };
+
 //5. Filtrar aquellos pacientes que indican ser de ISAPRE en la lista de consultas médicas de Dental (1 Punto)
 
-const pacientesIsapre = copiaDental.filter(paciente =>paciente.prevision == 'ISAPRE');
-console.log(pacientesIsapre)
+const isapre = copiaDental.filter(paciente => paciente.prevision == 'ISAPRE');
+// console.log(isapre)
+
+//Luego de filtrar los objetos que contienen la prevision isapre, agrupo sólo los pacientes y su previsión en un nuevo array
+const pacientesYPrevisionIsapre = [];
+
+isapre.forEach(paciente => {
+    pacientesYPrevisionIsapre.push({ paciente: paciente.paciente, prevision: paciente.prevision });
+});
+
+// console.log(pacientesYPrevisionIsapre);
 
 //6. Filtrar aquellos pacientes que indican ser de FONASA en la lista de consultas médicas de Traumatología (1 Punto).   
 
-const pacientesFonasa = copiaTraumatologia.filter(paciente =>paciente.prevision == 'FONASA');
-console.log(pacientesFonasa);
+const fonasa = copiaTraumatologia.filter(paciente => paciente.prevision == 'FONASA');
+// console.log(fonasa);
+
+//Luego de filtrar los objetos que contienen la prevision fonasa, agrupo sólo los pacientes y su previsión en un nuevo array
+const pacientesYPrevisionFonasa = [];
+
+fonasa.forEach(paciente => {
+    pacientesYPrevisionFonasa.push({ paciente: paciente.paciente, prevision: paciente.prevision });
+});
+
+// console.log(pacientesYPrevisionFonasa);
+
+//FUNCIÓN ÚNICA PARA IMPRIMIR
+//(comenté el código que se imprimía afuera para no desarmar el orden original del ejercicio)
+
+function imprimirManipulaciones() {
+    //Línea 1 que venia con el documento
+    document.write('<p>Estadisticas centro medico ñuñoa</p>');
+    //Ejercicio 3
+    document.write('<p>Lista de consultas médicas dentales</p>');
+
+    for (let i = 0; i < copiaDental.length; i++) {
+        document.write(`<p>${copiaDental[i].hora} - ${copiaDental[i].especialista} - ${copiaDental[i].paciente} -  ${copiaDental[i].rut} -  ${copiaDental[i].prevision} </p>`);
+    };
+    //Ejercicio 4
+    document.write(`<p>Total de pacientes atendidos: ${pacientesTotal.length}</p>`);
+
+    for (let i = 0; i < pacientesTotal.length; i++) {
+        document.write(`<p>${pacientesTotal[i]}</p>`)
+    };
+
+    //Ejercicio 5
+    document.write(`<p>Total de pacientes con ISAPRE en atenciones dentales: ${pacientesYPrevisionIsapre.length}</p>`);
+
+    for (let i = 0; i < pacientesYPrevisionIsapre.length; i++) {
+        document.write(`<p>${pacientesYPrevisionIsapre[i].paciente} - ${pacientesYPrevisionIsapre[i].prevision} </p>`)
+    }
+    //Ejercicio 6
+    document.write(`<p>Total de pacientes con FONASA en traumatología: ${pacientesYPrevisionFonasa.length}</p>`);
+
+    for (let i = 0; i < pacientesYPrevisionFonasa.length; i++) {
+        document.write(`<p>${pacientesYPrevisionFonasa[i].paciente} - ${pacientesYPrevisionFonasa[i].prevision} </p>`)
+    }
+
+}
+
+imprimirManipulaciones()
 
 //Agregar código para el desafio 2 aquí
 
